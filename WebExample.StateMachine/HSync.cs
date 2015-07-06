87f61ae4b02b6cad0b5e37b1819eea2185c8d02c
Serj-Tm <serj-tm@ya.ru>
2015-07-06 23:39:28 +0300

    WebExample.StateMachine

---
+++ WebExample.StateMachine/HSync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using SoftTech.Wui;
+
+namespace WebExample.StateMachine
+{
+  public class HSync : HWebSynchronizeHandler
+  {
+    public HSync()
+      : base(new Dictionary<string, Func<object, JsonData, HContext, HtmlResult<HElement>>> 
+        { 
+          { "default", Main.HView },
+          { "index", Main.HView },
+        })
+    {
+    }
+  }
+}
